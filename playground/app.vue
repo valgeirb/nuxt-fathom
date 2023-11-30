e3a889d0b05af4445d0f1044881d05a1ecbe3103
Valgeir Bjornsson <valgeir@hey.com>
2023-11-30 22:31:32 +0000

    👌 IMPROVE: cleanup

--- playground/app.vue
+++ playground/app.vue
@@ -1,24 +1,15 @@
 <template>
   <div>
     <nav>
-      <NuxtLink to="/"> Home </NuxtLink>
-      <NuxtLink to="/about"> About </NuxtLink>
-
-      <button @click="trackEvent('this is the event')">Track event</button>
+      <NuxtLink to="/">
+        Home
+      </NuxtLink>
+      <NuxtLink to="/about">
+        About
+      </NuxtLink>
     </nav>
     <NuxtPage />
   </div>
 </template>
 
-<script setup lang="ts">
-import { useFathom } from "#imports";
-
-const {
-  blockTrackingForMe,
-  enableTrackingForMe,
-  isTrackingEnabled,
-  setSite,
-  trackEvent,
-  trackPageview,
-} = useFathom();
-</script>
+<script setup lang="ts"></script>
